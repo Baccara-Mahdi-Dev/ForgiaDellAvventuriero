d22f5978845d54eb21eaebafc18644664ad079df
Baccara-Mahdi-Dev <baccara.dev+github@gmail.com>
2026-08-21 20:35:23 +0200

    Aggiornato il footer con info creatore e link per collaborare

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
+import { gameAnvil } from '@ng-icons/game-icons';
+import { simpleGithub } from '@ng-icons/simple-icons';
 import { TuiRoot } from '@taiga-ui/core';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, NgIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App {
+  readonly githubIcon = simpleGithub;
+  readonly authorIcon = gameAnvil;
+}
