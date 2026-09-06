06d1604f82b977c90e6f3fb502e20666516bcd16
Baccara-Mahdi-Dev <baccara.dev+github@gmail.com>
2026-09-06 12:46:51 +0200

    1.8.0 : - aggiunto sistema rapido di creazione rapida! - introduce repository e validazione dei dati persistiti; - centralizza regole di magie, armi e avanzamento; - alleggerisce wizard e store; - separa libreria e import/export; - aggiunge test di regressione; - corregge la cancellazione multipla asincrona; - formatting via npx prettier . --write

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,8 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { provideTaiga } from '@taiga-ui/core';
 import { App } from './app';
+
+const storageMock: Storage = {
+  length: 0,
+  clear: () => {},
+  getItem: () => null,
+  key: () => null,
+  removeItem: () => {},
+  setItem: () => {},
+};
+
 describe('App', () => {
   beforeEach(async () => {
+    Object.defineProperty(window, 'localStorage', {
+      configurable: true,
+      value: storageMock,
+    });
     Object.defineProperty(window, 'matchMedia', {
       configurable: true,
       value: () => ({
